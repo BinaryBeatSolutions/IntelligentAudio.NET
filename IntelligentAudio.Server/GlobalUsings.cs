60dc66de68ba200d86534eac2ac699e52aa56d57
GREENCOM <binarybeat@icloud.com>
2026-02-27 08:31:39 +0100

    Pipline startup Program

--- IntelligentAudio.Server/GlobalUsings.cs
+++ IntelligentAudio.Server/GlobalUsings.cs
@@ -3,3 +3,8 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+
+global using IntelligentAudio.Engine.Services;
--- IntelligentAudio.Server/Program.cs
+++ IntelligentAudio.Server/Program.cs
@@ -1,9 +1,14 @@
 ﻿
 
-namespace IntelligentAudio.Server;
-    public class Program
-    {
 
-        public static void Main(string[] args) { }
-    }
+var builder = Host.CreateApplicationBuilder(args);
 
+// Registrera pipelinen som en Singleton (det finns bara ett rör i hela systemet)
+builder.Services.AddSingleton<AudioPipeline>();
+
+// Här kommer vi senare lägga till:
+// builder.Services.AddHostedService<MicrophoneSource>();
+// builder.Services.AddHostedService<AudioEngine>();
+
+var host = builder.Build();
+await host.RunAsync();
