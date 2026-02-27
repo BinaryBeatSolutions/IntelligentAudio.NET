9bde0749f564c26998514fa46da2a513b811afb9
GREENCOM <binarybeat@icloud.com>
2026-02-27 11:11:27 +0100

    Smoke test succeded

--- IntelligentAudio.Server/Program.cs
+++ IntelligentAudio.Server/Program.cs
@@ -2,6 +2,9 @@
 
 
 
+using IntelligentAudio.Contracts.Events;
+using IntelligentAudio.Contracts.Models;
+
 var builder = Host.CreateApplicationBuilder(args);
 
 // Registrera pipelinen som en Singleton (det finns bara ett rör i hela systemet)
@@ -15,4 +18,24 @@ builder.Services.AddSingleton<IDawClientFactory, DefaultDawClientFactory>();
 // builder.Services.AddHostedService<AudioEngine>();
 
 var host = builder.Build();
+
+
+var eventAggregator = host.Services.GetRequiredService<IEventAggregator>();
+var clientFactory = host.Services.GetRequiredService<IDawClientFactory>();
+
+// Skapa en test-klient för Ableton på port 9001
+var testClientId = Guid.NewGuid();
+clientFactory.CreateClient(testClientId, 9001, "ableton");
+
+// Simulera att AI:n hittat ett ackord efter 2 sekunder
+_ = Task.Run(async () =>
+{
+    await Task.Delay(2000);
+    var testChord = new ChordInfo("Cmaj7", 0.99, DateTime.UtcNow);
+
+    // Publicerar eventet - detta ska trigga OscService!
+    eventAggregator.Publish(new ChordDetectedEvent(testClientId, testChord, DateTime.UtcNow));
+});
+// --- RÖKTEST SLUT ---
+
 await host.RunAsync();
